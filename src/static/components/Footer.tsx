2f75770384b69d6b0555ef76cdb68d7f9a235ca2
Charles Liu <liuchangyang112@gmail.com>
2021-07-24 01:43:37 +0800

    Add privacy policy to footer

--- src/static/components/Footer.tsx
+++ src/static/components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
                     <br />
                     Icons from thenounproject.com
                     <br />
-                    <a href="https://github.com/hongee/nasigoreng">Github</a> | <a target="_blank" href="https://www.facebook.com/groups/122880215014668/">Facebook</a> | <a href="mailto:exco@uclassa.org">Contact</a> | <a target="_blank" href="https://join.slack.com/t/ucla-ssa/shared_invite/zt-h5yndos4-BE0_AgcDMrDJ2DkLplkzdA">Slack</a> | <a target="_blank" href="https://mailchi.mp/868c856a80ab/join-our-ssa-mailing-list">Newsletter</a>
+                    <a href="https://www.termsfeed.com/live/4b8bde4a-10de-436a-aaf9-189b82bc192c">Privacy Policy</a> | <a href="https://github.com/hongee/nasigoreng">Github</a> | <a target="_blank" href="https://www.facebook.com/groups/122880215014668/">Facebook</a> | <a href="mailto:exco@uclassa.org">Contact</a> | <a target="_blank" href="https://join.slack.com/t/ucla-ssa/shared_invite/zt-h5yndos4-BE0_AgcDMrDJ2DkLplkzdA">Slack</a> | <a target="_blank" href="https://mailchi.mp/868c856a80ab/join-our-ssa-mailing-list">Newsletter</a>
                 </div>
             </div>
         </footer>
